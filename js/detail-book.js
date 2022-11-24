acd20b143e1d61398dc71681ef0f19547f08a0c5
alvinpradana <alvinpradana14@gmail.com>
2022-11-24 22:08:14 +0700

    update feature edit book title value

--- js/detail-book.js
+++ js/detail-book.js
@@ -1,6 +1,8 @@
 const titleBook = document.getElementById("bookTitle")
 const imgBrand = document.getElementById("imgBrand")
 const heroImg = document.getElementById("heroImg")
+const titleDelete = document.getElementById("titleDelete")
+const titleEdit = document.getElementById("titleEdit")
 
 const title = localStorage.getItem("img-title")
 const image = localStorage.getItem("image")
@@ -8,4 +10,6 @@ const image = localStorage.getItem("image")
 titleBook.innerText = title
 imgBrand.setAttribute("src", image)
 heroImg.style.backgroundImage = `url('.${image}')`
+titleDelete.innerText = title
+titleEdit.setAttribute("value", title)
 console.log(image);
